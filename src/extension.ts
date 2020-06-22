94f88ece2d3db509836680928433c125148b464a
shibayu36 <shibayu36@gmail.com>
2020-06-22 09:40:32 +0900

    Implements searchInCurrentFile

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,8 @@ export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand(
     "search-in-current-file.searchInCurrentFile",
-    () => {
-      vscode.window.showInformationMessage(
-        "Hello World from search-in-current-file!"
-      );
+    async () => {
+      await searchInCurrentFile();
     }
   );
 
@@ -18,3 +16,19 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
+
+async function searchInCurrentFile(): Promise<void> {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return;
+  }
+
+  const currentFilePath = vscode.workspace.asRelativePath(
+    activeEditor.document.uri
+  );
+  await vscode.commands.executeCommand("workbench.action.findInFiles", {
+    // Fill-in selected text to query
+    query: activeEditor.document.getText(activeEditor.selection),
+    filesToInclude: currentFilePath,
+  });
+}
