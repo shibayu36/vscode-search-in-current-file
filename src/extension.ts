8b32a9430cafe9e5f9c8634436bd36b8a6cad080
Peter Jiping Xie <peter.jp.xie@gmail.com>
2021-06-27 14:31:49 +1000

    Workaround for issue #9

--- src/extension.ts
+++ src/extension.ts
@@ -19,9 +19,17 @@ async function searchInCurrentFile(): Promise<void> {
     return;
   }
 
-  const currentFilePath = vscode.workspace.asRelativePath(
+  var currentFilePath = vscode.workspace.asRelativePath(
     activeEditor.document.uri
   );
+  
+  // Workaround for issue #9
+  // Take only basename if it is a Windows full path with colon (:), e.g. D:\a\b.txt -> b.txt 
+  if (currentFilePath.includes(":")) {
+    const path = require('path');
+    currentFilePath = path.basename(currentFilePath);
+  }
+
   await vscode.commands.executeCommand("workbench.action.findInFiles", {
     // Fill-in selected text to query
     query: activeEditor.document.getText(activeEditor.selection),
